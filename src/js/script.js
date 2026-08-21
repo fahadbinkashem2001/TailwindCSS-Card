a14dd7b5e6cd45c2bb24b88202bb64dc0fe15f99
Fahad Bin Kashem <fahadbinakshem2001@gmail.com>
2026-08-21 06:46:13 +0600

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,10 @@
+const updateButton = document.querySelector(".update-button");
+const deleteButton = document.querySelector(".delete-button");
+
+updateButton.addEventListener("click",function(){
+    alert("The Card Has Been Updated!");
+});
+
+deleteButton.addEventListener("click",function(){
+    alert("The Card Has Been Deleted!");
+});
